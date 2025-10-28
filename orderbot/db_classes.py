ae7588ebb9d86b1125c9ab6f500394519990cf6f
Zayus13 <sven_loechte@protonmail.com>
2025-10-28 16:18:47 +0100

    new room table

--- orderbot/db_classes.py
+++ orderbot/db_classes.py
@@ -44,9 +44,23 @@ class Cuts(Base):
     cid = Column(Integer, primary_key=True)
     pid = Column(Integer, ForeignKey("participants.pid"))
     oid = Column(Integer, ForeignKey("orders.oid"))
+    rid = Column(Integer, ForeignKey("rooms.rid"))
     cut = Column(Integer)
     name = Column(String)
     timestamp = Column(DATETIME, default=now())
+
+class Rooms(Base):
+    __tablename__ = "rooms"
+    rid = Column(Integer, primary_key=True)
+    room_id = Column(String, unique=True)
+    name = Column(String)
+    cuts = relationship(
+        "Cuts", backref="rooms", lazy=True, cascade="all,delete-orphan"
+    )
+    is_active = Column(Boolean, default=True)
+    is_dm = Column(Boolean, default=False)
+
+
 
 def setup_db(path: str) -> Session:
     engine = create_engine(path)
